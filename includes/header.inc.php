902ac98464264b295a04ac718645a00f184865f6
Tyson Reeder <t.reeder6@gmail.com>
2015-06-30 22:40:28 -0500

    Working on new design for website

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -3,28 +3,12 @@
   <div class="container-fluid">
     <div class="header-container">
       <div class="row">
-        <article class="logo-container col-xs-2">
-          <a href="/index.php">
-            <div class="logo pull-left">
-              <img src="images/logo.svg" />
-            </div>
-          </a>
-        </article>
-        <article class="title-container col-xs-8">
-          <section class="text-center uppercase">
-            <div class="title pt3">
-              <p class="focused">user focused</p> 
-              <p class="strategy-design">
-                <span class="strategy">strategy</span> 
-                + 
-                <span class="design">design</span>
-              </p>
-            </div>
-          </section>
+        <article>
+          <a href="index.php" class="uppercase">Tyson D Reeder</a>
         </article>
         <article class="menu-container">
           <nav class="pull-right touch-menu">
-            <a href="#" class="btn-menu"></a>
+            <a href="#" class="btn-menu">Menu</a>
           </nav>
         </article>
       </div>
--- index.php
+++ index.php
@@ -6,209 +6,34 @@
   <?php include("includes/header.inc.php"); ?>
   <?php include("includes/overlay.inc.php"); ?>
   <main id="work-overview">
-    <section class="container-fluid work-container bg-design-guide col-sm-12">
-      <div class="work-image col-md-5">
-        <figure class="desktop">
-          <img src="images/design-guide-desktop.png" />
-        </figure>
-        <figure class="device-md">
-          <img src="images/design-guide-md.png" />
-        </figure>
-        <figure class="device-sm">
-          <img src="images/design-guide-sm.png" />
-        </figure>
-      </div>
-      <article class="work-description col-sm-12 col-md-4 lazy">
-        <div class="description-container">
-          <div class="post-link white">
-            <h1 class="">Mutual of Omaha Design Guide</h1>
-            <p>Guiding principles for Mutual of Omaha design and experience across multiple environments.</p>
+    <section class="intro-container">
+      <article class="title-container">
+        <section class="text-center uppercase">
+          <div class="logo-container">
+            <div class="logo">
+              <img src="images/logo.svg" />
+            </div>
           </div>
-        </div>
-        <div class="btn-container">
-          <a class="btn-cta btn-left col-sm-6" href="work/design-guide/">
-            <span class="text">See Project</span>
-            <span class="arrow"><img src="images/arrow.svg" /></span>
-          </a>
-          <a class="btn-cta btn-right col-sm-6" href="http://www.mutualofomaha.com/design">
-            <span class="text no-underline">View Site</span>
-            <span class="arrow"><img src="images/arrow.svg" /></span>
-          </a>
-        </div>
+          <div class="title pt3">
+            <p class="focused">user focused</p> 
+            <p class="strategy-design">
+              <span class="strategy">strategy</span> 
+              + 
+              <span class="design">design</span>
+            </p>
+          </div>
+        </section>
       </article>
     </section>
-    <section class="container-fluid work-container bg-about-us col-sm-12">
-      <div class="work-image col-md-12">
-        <figure class="desktop">
-          <img src="images/about-us-desktop.png" />
-        </figure>
-        <figure class="device-md">
-          <img src="images/about-us-md.png" />
-        </figure>
-        <figure class="device-sm">
-          <img src="images/about-us-sm.png" />
-        </figure>
-      </div>
-      <article class="work-description col-md-12 lazy">
-        <div class="description-container col-md-8 col-md-offset-2">
-          <div class="post-link white">
-            <h1 class="">Mutual of Omaha About Us</h1>
-            <p>Design and strategy for Mutual of Omaha About Us Page.</p>
-          </div>
-        </div>
-      </article>
-      <div class="btn-container">
-        <a class="btn-cta btn-left col-sm-6" href="work/design-guide/">
-          <span class="text no-underline">See Project</span>
-          <span class="arrow"><img src="images/arrow.svg" /></span>
-        </a>
-        <a class="btn-cta btn-right col-sm-6" href="http://www.mutualofomaha.com/design">
-          <span class="text no-underline">View Site</span>
-          <span class="arrow"><img src="images/arrow.svg" /></span>
-        </a>
-      </div>  
-    </section>
-    <section class="container-fluid work-container bg-design-guide col-sm-12">
-      <div class="work-image col-md-5">
-        <figure class="desktop">
-          <img src="images/design-guide-desktop.png" />
-        </figure>
-        <figure class="device-md">
-          <img src="images/design-guide-md.png" />
-        </figure>
-        <figure class="device-sm">
-          <img src="images/design-guide-sm.png" />
-        </figure>
-      </div>
-      <article class="work-description col-sm-12 col-md-4 lazy">
-        <div class="description-container">
-          <div class="post-link white">
-            <h1 class="">Mutual of Omaha Design Guide</h1>
-            <p>Guiding principles for Mutual of Omaha design and experience across multiple environments.</p>
-          </div>
-        </div>
-        <div class="btn-container">
-          <a class="btn-cta btn-left col-sm-6" href="work/design-guide/">
-            <span class="text">See Project</span>
-            <span class="arrow"><img src="images/arrow.svg" /></span>
-          </a>
-          <a class="btn-cta btn-right col-sm-6" href="http://www.mutualofomaha.com/design">
-            <span class="text no-underline">View Site</span>
-            <span class="arrow"><img src="images/arrow.svg" /></span>
-          </a>
-        </div>
-      </article>
-    </section>
-    <section class="container-fluid work-container bg-bsd col-sm-12">
-      <div class="work-image col-md-12">
-        <figure class="desktop">
-          <img src="images/bsd-reporting-desktop.png" />
-        </figure>
-        <figure class="device-md animate">
-          <img src="images/bsd-reporting-md.png" />
-        </figure>
-        <figure class="device-sm animate">
-          <img src="images/bsd-reporting-sm.png" />
-        </figure>
-      </div>
-      <article class="work-description col-md-12 lazy">
-        <div class="description-container col-md-8 col-md-offset-2">
-          <div class="post-link white">
-            <h1 class="">Mutual of Omaha BSD Reporting</h1>
-            <p>An application that allows sales reps on the road access to information only accessible on Mutual's campus.</p>
-          </div>
-        </div>
-      </article>
-      <div class="btn-container">
-        <a class="btn-cta btn-left col-sm-6" href="work/design-guide/">
-          <span class="text no-underline">See Project</span>
-          <span class="arrow"><img src="images/arrow.svg" /></span>
-        </a>
-        <a class="btn-cta btn-right col-sm-6" href="http://www.mutualofomaha.com/design">
-          <span class="text no-underline">View Site</span>
-          <span class="arrow"><img src="images/arrow.svg" /></span>
-        </a>
+    <section class="container-fluid work-container about-us col-sm-12">
+      <div class="bg-frame">
+        <img class="about-us-bg" src="/images/bg-about-us.png" />
       </div>
     </section>
-    <section class="container-fluid work-container bg-dgr col-sm-12">
-      <div class="work-image">
-        <figure>
-          <img src="images/dgr-logo.svg" />
-        </figure>
+    <section class="container-fluid work-container about-us2 col-sm-12">
+      <div class="bg-frame">
+        <img class="about-us-bg" src="/images/bg-about-us.png" />
       </div>
-      <article class="work-description col-md-12 lazy">
-        <div class="description-container col-md-8 col-md-offset-2">
-          <div class="post-link white">
-            <h1 class="">Darden-Gloeb-Reeder Brand Refresh</h1>
-            <p>Complete rebrand for Omaha painting contractors, Darden-Gloeb-Reeder, Inc.</p>
-          </div>
-        </div>
-      </article>
-      <div class="btn-container">
-        <a class="btn-cta btn-left col-sm-6" href="work/dgr-brand-refresh/">
-          <span class="text no-underline">See Project</span>
-          <span class="arrow"><img src="images/arrow.svg" /></span>
-        </a>
-        <a class="btn-cta btn-right col-sm-6" href="http://www.dgrpainting.com" target="_blank">
-          <span class="text no-underline">View Site</span>
-          <span class="arrow"><img src="images/arrow.svg" /></span>
-        </a>
-      </div>
-    </section>  
-<!--     <section class="container-fluid work-container bg-iconography col-sm-6">
-      <div class="work-image col-md-12">
-        <figure>
-          <img src="" />
-        </figure>
-      </div>
-      <article class="work-description col-md-8 col-md-offset-2 lazy">
-        <div class="description-container">
-          <div class="post-link white">
-            <h1 class="">Miscellaneous Iconography</h1>
-            <a class="btn btn-cta btn-view-project" href="#">
-              <span class="text">See Project</span>
-              <span class="pipe"></span>
-              <span class="arrow"><img src="images/arrow.svg" /></span>
-            </a>
-            <a class="btn btn-cta btn-view-site" href="#">
-              <span class="text">View Site</span>
-              <span class="pipe"></span>
-              <span class="arrow"><img src="images/arrow.svg" /></span>
-            </a>
-          </div>
-        </div>
-      </article>
-    </section> -->
-<!--     <section class="test container-fluid work-container bg-iconography col-sm-6">
-      <div class="work-image col-md-12">
-        <figure class="desktop">
-          <img src="images/design-guide-desktop.png" />
-        </figure>
-        <figure class="device-md animate">
-          <img src="images/design-guide-md.png" />
-        </figure>
-        <figure class="device-sm animate">
-          <img src="images/design-guide-sm.png" />
-        </figure>
-      </div>
-      <article class="work-description col-md-8 col-md-offset-2 lazy">
-        <div class="description-container">
-          <div class="post-link white">
-            <h1 class="">Miscellaneous Iconography</h1>
-            <p class="description">Guiding principles for Mutual of Omaha design and experience across multiple environments.</p>
-            <a class="btn btn-cta btn-view-project" href="#">
-              <span class="text">See Project</span>
-              <span class="pipe"></span>
-              <span class="arrow"><img src="images/arrow.svg" /></span>
-            </a>
-            <a class="btn btn-cta btn-view-site" href="#">
-              <span class="text">View Site</span>
-              <span class="pipe"></span>
-              <span class="arrow"><img src="images/arrow.svg" /></span>
-            </a>
-          </div>
-        </div>
-      </article>
-    </section> -->
+    </section>
   </main>
   <?php include("includes/footer.inc.php"); ?>
